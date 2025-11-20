53b3c2b67b86425e5c2e5eecdabd91fd2acb1272
Destiny Computers <jatinxraftaar@example.com>
2025-11-20 21:29:53 +0530

    first commit Shub aram

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+import "./App.css";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+
+function App() {
+  const [selectedTab, setselectedTab] = useState("Home");
+  
+
+  return (
+    <div className="app-container">
+      <SideBar selectedTab={selectedTab} setselectedTab={setselectedTab}/>
+      <div className="content">
+        <Header />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
